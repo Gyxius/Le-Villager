39975adfa8aaf14dbbb84309b0e1756d530eff43
Gyxius <mitsou.fortunat@gmail.com>
2024-04-13 18:51:16 +0200

    Add attack system The player can attack the enemy and vice versa

--- Character.py
+++ Character.py
@@ -32,20 +32,14 @@ class Character(pygame.sprite.Sprite):
         self.current_time = pygame.time.get_ticks()  # Get current time
         self.speed = 1000
 
-    def attack(self):
-        pass
-
-    def defend(self):
-        pass
-
     def move(self):
         pass
 
     def movement_speed(func):
-        def inner(self):
+        def inner(self, player):
             current_time = pygame.time.get_ticks()
             if current_time - self.current_time >= self.speed:
-                func(self)
+                func(self, player)
                 self.current_time = current_time
         return inner
 
@@ -73,6 +67,7 @@ class Player(Character):
     """
     def __init__(self, group, name = "Bob", pos_x = 0, pos_y = 0):
         Character.__init__(self)
+        self.spritetype = "player" 
         self.image = Loader.load('img/knight.png', 'LEFT')
         self.rect = self.image.get_rect()
         self.playerx = pos_x*TILE_SIZE
@@ -83,22 +78,24 @@ class Player(Character):
         self.rect.y = self.playery
         self.name = name
         self.group = group
+        self.health = 100
+        self.attack = 30
 
     def move(self, event):
-        if event.type == KEYDOWN:
-            if event.key == K_q:
-                self.playerdx = -TILE_SIZE
-            elif event.key == K_d:
-                self.playerdx = TILE_SIZE 
-            
-            if self.playerdx < 0:
-                self.image = Loader.load('img/knight.png', 'LEFT')
-            elif self.playerdx > 0:
-                self.image = Loader.load('img/knight.png', 'RIGHT')
-            else:
-                pass
-            self.checkCollision()
-            self.playerx += self.playerdx 
+        if event.key == K_q:
+            self.playerdx = -TILE_SIZE
+        elif event.key == K_d:
+            self.playerdx = TILE_SIZE 
+        
+        if self.playerdx < 0:
+            self.image = Loader.load('img/knight.png', 'LEFT')
+        elif self.playerdx > 0:
+            self.image = Loader.load('img/knight.png', 'RIGHT')
+        else:
+            pass
+        self.checkCollision()
+        self.playerx += self.playerdx 
+        self.playerdx = 0
 
     def update(self):
         self.rect.x  = self.playerx
@@ -111,9 +108,11 @@ class Player(Character):
         requested_rect = pygame.Rect.copy(self.rect)
         requested_rect.x += self.playerdx
         for sprite in self.group:
-            print(sprite)
             if sprite.rect.colliderect(requested_rect):
                 self.playerdx = 0
+    
+    def getDamage(self, enemy):
+        self.health -= enemy.attack
 
 
 class Enemy(Character):
@@ -146,6 +145,7 @@ class Enemy(Character):
     """
     def __init__(self, group):
         Character.__init__(self)
+        self.spritetype = "enemy" 
         self.image = Loader.load('img/skeleton.png', 'LEFT')
         self.rect = self.image.get_rect()
         self.enemyx = 7*TILE_SIZE
@@ -154,30 +154,35 @@ class Enemy(Character):
         self.enemydy = 0
         self.rect.x = self.enemyx
         self.rect.y = self.enemyy
-        self.state = "Neutral"
-        self.speed = 1000
+        self.state = "Idle"
+        self.speed = 1000 #in ms
         self.group = group
+        self.health = 100
+        self.attack = 10
 
     @Character.movement_speed
-    def move(self):
+    def move(self, player):
         """
         Change the position of the character
         The enemy either moves randomly if the state is "Neutral" or toward the player if "Attacked"
         """
-        if self.state == "Neutral":
+        if self.state == "Idle":
             self.enemydx = random.randint(-1,1) * TILE_SIZE 
-            if self.enemydx > 0:
-                self.image = Loader.load('img/skeleton.png', 'RIGHT')
-            elif self.enemydx < 0:
-                self.image = Loader.load('img/skeleton.png', 'LEFT')
-            else:
-                pass
-            self.checkCollision()
-            self.enemyx += self.enemydx 
 
-        if self.state == "Attacked":
-            ## TODO update by adding the player, or actually using the group
-            pass
+        elif self.state == "Attacked":
+            self.enemydx = self.follow(player) * TILE_SIZE 
+        
+        if self.enemydx > 0:
+            self.image = Loader.load('img/skeleton.png', 'RIGHT')
+        elif self.enemydx < 0:
+            self.image = Loader.load('img/skeleton.png', 'LEFT')
+        else:
+            if self.state == "Attacked":
+                player.getDamage(self)
+
+        self.checkCollision()
+        self.enemyx += self.enemydx 
+        self.enemydx = 0
 
     def checkCollision(self):
         requested_rect = pygame.Rect.copy(self.rect)
@@ -185,10 +190,29 @@ class Enemy(Character):
         for sprite in self.group:
             if sprite.rect.colliderect(requested_rect):
                 self.enemydx = 0
-                
-    def update(self, a = 2):
-        self.move()
+    
+    def follow(self, player):
+        if player.playerx + TILE_SIZE <= self.enemyx <= player.playerx + 2*TILE_SIZE:
+            return 0
+        elif player.playerx < self.enemyx  :
+            return -1
+        elif player.playerx > self.enemyx  :
+            return 1
+        else:
+            return 0
+    
+    def getDamage(self, player):
+        if self.state == "Idle":
+            self.state = "Attacked"
+        self.health -= player.attack
+
+    def checkDeath(self):
+        if self.health < 0:
+            self.kill()
+        
+    def update(self):
         self.rect.x  = self.enemyx
         self.rect.y  = self.enemyy
+        self.checkDeath()
 
         
--- main.py
+++ main.py
@@ -15,24 +15,48 @@ class Game:
         self.screen.fill(LIGHT_BLUE)
         self.map = Map()
         self.character_group = pygame.sprite.Group()
+        self.attackable_group = pygame.sprite.Group()
+
         self.player1 = Player(self.character_group, "Bob", 5, 7)
         self.enemy1 = Enemy(self.character_group)
+        self.attackable_group.add(self.enemy1)
         self.character_group.add(self.enemy1)
         self.character_group.add(self.player1)
         self.clock = pygame.time.Clock()
 
     def update(self):
+        self.playerUpdate()
+        self.enemyUpdate()
+        self.character_group.update()
+
+    def playerUpdate(self):
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.display.quit()
                 sys.exit()
-            else:
-                self.player1.move(event)
+            if event.type == KEYDOWN:
+                if event.key == K_q or event.key == K_d:
+                    self.player1.move(event)
+                if event.key == K_SPACE:
+                    self.playerAttack()
+        
+    def playerAttack(self):
+        """ Check if enemy is nearby and attacks it"""
+        left_rect = pygame.Rect.copy(self.player1.rect)
+        left_rect.x -= TILE_SIZE
+        right_rect = pygame.Rect.copy(self.player1.rect)
+        right_rect.x += TILE_SIZE
+        for target_sprite in self.attackable_group:
+            if target_sprite.rect.colliderect(left_rect) or target_sprite.rect.colliderect(right_rect):
+                target_sprite.getDamage(self.player1)
+
+    def enemyUpdate(self):
+        #TODO use attackble group and update this group instead
+        self.enemy1.move(self.player1)
 
     def draw(self):
         self.screen.fill(LIGHT_BLUE)
         self.map.drawGrid(self.screen)
-        self.character_group.update()
         self.character_group.draw(self.screen)
         pygame.display.update()
 
